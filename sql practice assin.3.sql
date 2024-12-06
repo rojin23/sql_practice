e7cc3d887d51e603645465e4d1ea1d3b5389e893
wazza2373 <rojinkc20@gmail.com>
2024-12-06 21:58:31 +0545

    Add files via upload

---
+++ sql practice assin.3.sql
@@ -0,0 +1,9 @@
+create schema school;
+use school;
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
